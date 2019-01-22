67f4965f046956f70ec293af4a54457f41fe9a41
Paul Richards <sheff.pdr@gmail.com>
2019-01-22 09:51:41 +0000

    add filter and sort actions to redux

--- src/redux/display/index.js
+++ src/redux/display/index.js
@@ -1,3 +1,14 @@
+export const CHANGE_FILTER = 'hotels/CHANGE_FILTER';
+export const CHANGE_SORT = 'hotels/CHANGE_SORT';
+
+export const changeFilter = (filterString) => {
+    return { type: CHANGE_FILTER, filterString };
+};
+
+export const changeSort = (sortMethod) => {
+    return { type: CHANGE_SORT, sortMethod };
+};
+
 export const initialState = {
     filterString: '',
     sortMethod: 'none',
@@ -5,6 +16,16 @@ export const initialState = {
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
+        case CHANGE_FILTER:
+            return {
+                ...state,
+                filterString: action.filterString,
+            }
+        case CHANGE_SORT:
+            return {
+                ...state,
+                sortMethod: action.sortMethod,
+            }
         default:
             return state;
     }
--- src/redux/display/tests/index.test.js
+++ src/redux/display/tests/index.test.js
@@ -8,4 +8,17 @@ describe('reducer', () => {
     expect(result).toEqual(mockState);
   });
 
+  it('should update the filterString on CHANGE_FILTER', () => {
+    const mockString = 'free bar';
+    const mockAction = changeFilter(mockString);
+    const result = reducer(initialState, mockAction);
+    expect(result.filterString).toEqual(mockString);
+  });
+
+  it('should update the sortMethod on CHANGE_SORT', () => {
+    const mockString = 'anywayyoulike';
+    const mockAction = changeSort(mockString);
+    const result = reducer(initialState, mockAction);
+    expect(result.sortMethod).toEqual(mockString);
+  });
 });
