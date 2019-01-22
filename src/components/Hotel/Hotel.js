46628d30c32cff2f5def36b73ea256c9453d633d
Paul Richards <sheff.pdr@gmail.com>
2019-01-22 09:23:03 +0000

    build hotel component

--- src/components/Hotel/Hotel.js
+++ src/components/Hotel/Hotel.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Hotel.css';
 
 export const Hotel = ({ hotel = {} }) => {
     return (
         <div className="Hotel">
-            This is {hotel.name}
+            <div className="Hotel-left">
+                <div className="Hotel-name">{hotel.name}</div>
+                <div className="Hotel-rating">Rating: {hotel.starRating}</div>
+            </div>
+            <div className="Hotel-right">
+                <div className="Hotel-facilities-title">Facilities:</div>
+                <div>
+                    facilities go here
+                </div>
+            </div>
         </div>
     )
+}
+
+Hotel.propTypes = {
+    hotel: PropTypes.object,
 }
 
 export default Hotel;
