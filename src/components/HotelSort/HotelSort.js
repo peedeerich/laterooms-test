a2afee4361602191bb5f7116be682fe97a859b93
Paul Richards <sheff.pdr@gmail.com>
2019-01-22 10:20:47 +0000

    hook up sort to redux

--- src/components/HotelSort/HotelSort.js
+++ src/components/HotelSort/HotelSort.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import { changeSort } from '../../redux/display';
 
 export const HotelSort = ( { onChangeSort = () => {} }) => (
     <select name="sort" size="0" onChange={event => onChangeSort(event.target.value)}>
@@ -13,4 +16,8 @@ HotelSort.propTypes = {
     onChangeSort: PropTypes.func,
 }
 
-export default HotelSort;
+const mapDispatchToProps = dispatch => ({
+    onChangeSort: sortMethod => dispatch(changeSort(sortMethod)),
+})
+
+export default connect(null, mapDispatchToProps)(HotelSort);
