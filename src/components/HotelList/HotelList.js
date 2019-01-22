408bd0515bb2fdcf09d970660e0d00c2b5837271
Paul Richards <sheff.pdr@gmail.com>
2019-01-22 10:06:47 +0000

    filter hotels in the list component based on state

--- src/components/HotelList/HotelList.js
+++ src/components/HotelList/HotelList.js
@@ -2,27 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
+import { filterByFacilities } from '../../utils/filterHotels';
+
 import Hotel from '../Hotel';
 
-function renderHotels(hotels) {
-    return hotels.map((hotel, index) => {
+function renderHotels(hotels, facility) {
+    const filteredHotels = filterByFacilities(hotels, facility);
+    return filteredHotels.map((hotel, index) => {
         return <Hotel hotel={hotel} key={index}/>
     });
 }
 
-export const HotelList = ({ hotels = [] }) => (
+export const HotelList = ({ hotels = [], facility = '' }) => (
     <div>
         <div>We found {hotels.length} hotels</div>
-        {renderHotels(hotels)}
+        {renderHotels(hotels, facility)}
     </div>
 )
 
 HotelList.propTypes = {
     hotels: PropTypes.arrayOf(PropTypes.object),
+    facility: PropTypes.string,
 }
 
 const mapStateToProps = state => ({
     hotels: state.hotels.hotels,
+    facility: state.display.filterString,
 })
 
 export default connect(mapStateToProps)(HotelList);
