72e1f67cf6c023398415f0b40e6fc79b83056417
Paul Richards <sheff.pdr@gmail.com>
2019-01-22 08:40:41 +0000

    store hotels action

--- src/redux/hotels/index.js
+++ src/redux/hotels/index.js
@@ -1,12 +1,25 @@
+import fetchHotels from '../../mockServer/fetchHotels';
+
+export const STORE_HOTELS = 'hotels/STORE_HOTELS';
+
+export const storeHotels = (hotels) => {
+  return { type: STORE_HOTELS, hotels };
+};
+
 export const initialState = {
     hotels: [],
 };
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
+      case STORE_HOTELS:
+        return {
+            ...state,
+            hotels: action.hotels
+        }
       default:
         return state;
     }
-  };
+};
 
 export default reducer;
--- src/redux/hotels/tests/index.test.js
+++ src/redux/hotels/tests/index.test.js
@@ -1,4 +1,4 @@
-import reducer from '..';
+import reducer, { storeHotels, initialState } from '..';
 
 describe('reducer', () => {
   it('should return state for unknown action', () => {
@@ -6,5 +6,23 @@ describe('reducer', () => {
     const mockAction = { type: '???' };
     const result = reducer(mockState, mockAction);
     expect(result).toEqual(mockState);
+  });
+
+  it('should store hotels on state on STORE_HOTELS', () => {
+    const hotels = [
+            {
+                "name": "hotelone",
+                "starRating": 5,
+                "facilities": ["car park", "pool"]
+            },
+            {
+                "name": "hoteltwo",
+                "starRating": 3,
+                "facilities": ["car park", "gym"]
+            },
+        ];
+    const mockAction = storeHotels(hotels);
+    const result = reducer(initialState, mockAction);
+    expect(result.hotels).toEqual(hotels);
   });
 });
